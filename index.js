be60b4d61b0706465fe2917adfbcae3007d48ebf
Alex TEA <alextea2@gmail.com>
2020-06-26 00:10:59 +0200

    search

--- index.js
+++ index.js
@@ -1,61 +1,7 @@
 const searchBtn = document.querySelector('#search_manga');
 const input = document.querySelector('.search-field');
-
-searchBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const value = input.value;
-  console.log('click')
-});
-
-const search = async () => {
-  const url = new URL("https://query.wikidata.org/sparql");
-  const query = "PREFIX bd: <http://www.bigdata.com/rdf#>\
-                  PREFIX wikibase: <http://wikiba.se/ontology#>\
-                  PREFIX wd: <http://www.wikidata.org/entity/>\
-                  PREFIX wdt: <http://www.wikidata.org/prop/direct/>\
-                  select ?objectLabel ?objectDescription ?logo ?genreLabel ?tomes ?episodes \
-                  where {\
-                  ?object wdt:P31 wd:Q21198342 .\
-                  OPTIONAL{\
-                  ?object wdt:P154 ?logo;\
-                  }\
-                  ?object wdt:P136 ?genre.\
-                    ?object wdt:P2635 ?tomes.\
-                    OPTIONAL{\
-                      ?object wdt:P1113 ?episodes.\
-                      }\
-                  SERVICE wikibase:label {\
-                  bd:serviceParam wikibase:language \"en,fr\" .\
-                  }\
-                  }\
-                  ORDER BY ?objectLabel\
-                  LIMIT 30"
-  const params = {
-    queryLN: 'SPARQL',
-    query: query
-  }
-  url.search = new URLSearchParams(params).toString();
-
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "accept": "application/sparql-results+json"
-      }
-    });
-    const responseData = await response.json();
-    responseData.results.bindings.forEach(manga => {
-      createCard(manga);
-    })
-
-    console.log(responseData);
-  } catch (e) {
-    console.error(e);
-  }
-}
-
 const results = document.querySelector('.results');
+
 
 const createCard = (data) => {
 
@@ -99,9 +45,131 @@ const createCard = (data) => {
     container.appendChild(episodes);
   }
 
-  results.appendChild(container);
+  return container;
 }
 
-search();
+
+searchBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const text = input.value;
+  if (text === "") {
+    getAll();
+    return;
+  }
+  const responseData = await search(text);
+  results.innerHTML = "";
+  if (responseData.results.bindings.length === 0) {
+    const error = document.createElement('p');
+    error.style.color = 'white';
+    error.innerText = 'No results found for this search';
+    results.appendChild(error);
+    return;
+  }
+  const card = createCard(responseData.results.bindings[0]);
+  results.appendChild(card) ;
+
+});
+
+const search = async (text) => {
+  const url = new URL("https://query.wikidata.org/sparql");
+  const query = `SELECT DISTINCT ?objectLabel ?objectDescription (sample(?logos) as ?logo) (GROUP_CONCAT(DISTINCT ?genreLabels ; separator=", ") as ?genreLabel) (MAX(?tome) as ?tomes ) (MAX(?episode) as ?episodes) (GROUP_CONCAT(DISTINCT ?charLabel ; separator=", ") as ?char)
+                  WHERE{
+                    ?object wdt:P31/wdt:P279* wd:Q8274 ;
+                          rdfs:label ?objectLabel ;
+                          schema:description ?objectDescription;
+                          wdt:P136 ?genre;
+                    OPTIONAL{
+                      ?object wdt:P2635 ?tome.
+                      }
+                    OPTIONAL{
+                      ?object wdt:P154 ?logos.
+                      }
+                     OPTIONAL{
+                      ?object wdt:P1113 ?episode.
+                      }
+                    OPTIONAL{
+                      ?object wdt:P674 ?characters;
+                              }
+                    FILTER(CONTAINS(LCASE(?objectLabel), "${text}"))
+                    FILTER (LANG(?objectLabel)="en")
+                    FILTER (LANG(?objectDescription)="en")
+                    SERVICE wikibase:label {
+                      bd:serviceParam wikibase:language "en,fr" .
+                      ?genre rdfs:label ?genreLabels .
+                      ?characters rdfs:label ?charLabel .
+                  }
+                  }
+                  group by ?objectLabel ?objectDescription
+                  order by ?objectLabel
+                  limit 10`
+  const params = {
+    queryLN: 'SPARQL',
+    query: query
+  }
+  url.search = new URLSearchParams(params).toString();
+  try {
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "accept": "application/sparql-results+json"
+      }
+    });
+    const responseData = await response.json();
+    return responseData;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const getAll = async () => {
+  const url = new URL("https://query.wikidata.org/sparql");
+  const query = "PREFIX bd: <http://www.bigdata.com/rdf#>\
+                  PREFIX wikibase: <http://wikiba.se/ontology#>\
+                  PREFIX wd: <http://www.wikidata.org/entity/>\
+                  PREFIX wdt: <http://www.wikidata.org/prop/direct/>\
+                  select ?objectLabel ?objectDescription ?logo ?genreLabel ?tomes ?episodes \
+                  where {\
+                  ?object wdt:P31 wd:Q21198342 .\
+                  OPTIONAL{\
+                  ?object wdt:P154 ?logo;\
+                  }\
+                  ?object wdt:P136 ?genre.\
+                    ?object wdt:P2635 ?tomes.\
+                    OPTIONAL{\
+                      ?object wdt:P1113 ?episodes.\
+                      }\
+                  SERVICE wikibase:label {\
+                  bd:serviceParam wikibase:language \"en,fr\" .\
+                  }\
+                  }\
+                  ORDER BY ?objectLabel\
+                  LIMIT 30"
+  const params = {
+    queryLN: 'SPARQL',
+    query: query
+  }
+  url.search = new URLSearchParams(params).toString();
+
+  try {
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "accept": "application/sparql-results+json"
+      }
+    });
+    const responseData = await response.json();
+    responseData.results.bindings.forEach(manga => {
+      const card = createCard(manga);
+      results.appendChild(card);
+    })
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+
+getAll();
 
 
